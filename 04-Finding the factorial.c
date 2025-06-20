9875b313a280d301cce466eca36bcd1962ae077d
SK <englishenglishenglish@proton.me>
2025-06-20 15:38:41 +0000

    Update 04-Finding the factorial.c

--- 04-Finding the factorial.c
+++ 04-Finding the factorial.c
@@ -1,18 +1,24 @@
 #include<stdio.h>
 
+//logic for fact function
 int fact(int n){
     if(n==0)return 1;
 
     return n*fact(n-1);
 }
+
+
 int main(){
     int n;
     scanf("%d",&n);
+    //logic for special case
     if(n<0 || n>12){
         printf("Invalid input\n");
         return -1;
     }   
-    
+
+
+    //logic for ouput
     printf("The factorial of %d is %d\n",n,fact(n));
     return 0;
 }
